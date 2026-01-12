8775c3526d13caed60a21c5d0e15a1bc5b8135de
Sprite <sprite@meet-mesh.local>
2026-01-12 11:17:00 +0000

    feat(api): implement poll vote handlers
    Add handler_votes.go with the following functionality:
    - SubmitVote: Allows guests to vote on polls
    - GetLinkVotes: Returns votes for a poll (owner only)
    - GetPollResults: Returns public poll results with tally
    - PickPollWinner: Picks the winning slot and closes the poll
    
    Also adds SendPollWinner stub method to Mailer placeholder.

--- api/handler.go
+++ api/handler.go
@@ -31,3 +31,32 @@ func NewHandler(db *gorm.DB, auth *AuthService, caldav *CalDAVClient, mailer *Ma
 
 // Placeholder type for Mailer that will be implemented later
 type Mailer struct{}
+
+// SendPollWinner sends notification about the poll winner to all voters
+func (m *Mailer) SendPollWinner(link *Link, slot *Slot, votes []Vote) {
+	// TODO: Implement email notification
+}
+
+// SendBookingConfirmation sends confirmation email to guest
+func (m *Mailer) SendBookingConfirmation(booking *Booking, link *Link) error {
+	// TODO: Implement email sending
+	return nil
+}
+
+// SendBookingPending sends pending approval notification to guest
+func (m *Mailer) SendBookingPending(booking *Booking, link *Link) error {
+	// TODO: Implement email sending
+	return nil
+}
+
+// SendBookingApproved sends approval notification to guest
+func (m *Mailer) SendBookingApproved(booking *Booking, link *Link) error {
+	// TODO: Implement email sending
+	return nil
+}
+
+// SendBookingDeclined sends decline notification to guest
+func (m *Mailer) SendBookingDeclined(booking *Booking, link *Link) error {
+	// TODO: Implement email sending
+	return nil
+}
